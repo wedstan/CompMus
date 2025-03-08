f8fc3def3c1870cbdbc24b9f60a166b1a54437e9
wedstan <wed0e3e@gmail.com>
2025-03-08 13:27:18 +0100

    Add files via upload

--- compmus.R
+++ compmus.R
@@ -233,7 +233,7 @@ compmus_self_similarity <- function(dat, feature, distance = "euclidean") {
 #' major_chord <-
 #'   c(1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0)
 #' minor_chord <-
-#'   c(1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0)
+#'   c(1, 0, 0, 1, 0, , 0, 1, 0, 0, 0, 0)
 #' chord_templates <-
 #'   tribble(
 #'     ~name, ~template,
@@ -258,27 +258,72 @@ compmus_self_similarity <- function(dat, feature, distance = "euclidean") {
 #'       )
 #'   ) %>%
 #'   compmus_match_pitch_template(chord_templates, "euclidean", "manhattan")
-compmus_match_pitch_template <- function(dat, templates, method = "cosine", norm = "euclidean") {
-  compmus_long_distance(
-    dat %>%
-      dplyr::mutate(
-        pitches = purrr::map(pitches, compmus_normalise, norm)
-      ),
-    templates %>%
-      dplyr::transmute(
-        start = name,
-        duration = 1,
-        pitches = purrr::map(template, compmus_normalise, norm)
-      ),
-    feature = pitches,
-    method = method
-  ) %>%
-    dplyr::transmute(
-      start = xstart,
-      duration = xduration,
-      name = factor(ystart, levels = purrr::pluck(templates, "name")),
-      d
+compmus_match_pitch_templates <- function(dat, templates, norm = "euclidean", distance = "cosine") {
+
+    ## Supported functions
+
+  manhattan <- function(x, y) sum(abs(x - y))
+  euclidean <- function(x, y) sqrt(sum((x - y)^2))
+  chebyshev <- function(x, y) max(abs(x - y))
+  pearson <- function(x, y) 1 - cor(x, y)
+  cosine <- function(x, y) {
+    1 - sum(compmus_normalise(x, "euc") * compmus_normalise(y, "euc"))
+  }
+  angular <- function(x, y) 2 * acos(1 - cosine(x, y)) / pi
+  aitchison <- function(x, y) {
+    euclidean(compmus_normalise(x, "clr"), compmus_normalise(y, "clr"))
+  }
+
+  ## Method aliases
+
+  METHODS <-
+    list(
+      manhattan = manhattan,
+      cityblock = manhattan,
+      taxicab = manhattan,
+      L1 = manhattan,
+      totvar = manhattan,
+      euclidean = euclidean,
+      L2 = euclidean,
+      chebyshev = chebyshev,
+      maximum = chebyshev,
+      pearson = pearson,
+      correlation = pearson,
+      cosine = cosine,
+      angular = angular,
+      aitchison = aitchison
     )
+
+  ## Function selection
+
+  if (!is.na(i <- pmatch(distance, names(METHODS)))) {
+    dplyr::cross_join(
+      dat |>
+        tidyr::nest(.by = time) |>
+        dplyr::transmute(
+          time,
+          x =
+            purrr::map(
+              data,
+              \(df) df |> dplyr::arrange(pc) |> dplyr::pull(value)
+            )
+        ) |>
+        dplyr::arrange(time) |>
+        dplyr::filter(dplyr::row_number(time) %% 10 == 5),
+      templates |>
+        dplyr::transmute(
+          name,
+          y = purrr::map(template, \(v) compmus_normalise(v, norm))
+        )
+      ) |>
+      dplyr::transmute(
+        time,
+        name = factor(name, levels = purrr::pluck(templates, "name")),
+        d = purrr::map2_dbl(x, y, METHODS[[i]])
+      )
+  } else {
+    stop("The distance name is ambiguous or the method is unsupported.")
+  }
 }
 
 circshift <- function(v, n) {
@@ -291,7 +336,7 @@ compmus_chroma <- function(file, norm = "identity") {
     purrr::map(
       \(v) {
         v |>
-          circshift(1) |>
+          circshift(-8) |>
           matrix(3, 12) |>
           colMeans() |>
           compmus_normalise(method = norm) |>
@@ -318,3 +363,84 @@ compmus_mfccs <- function(file, norm = "identity") {
     dplyr::mutate(time = 1024 * time / 44100)
 }
 
+#      C     C#    D     Eb    E     F     F#    G     Ab    A     Bb    B
+major_chord <-
+  c(   1,    0,    0,    0,    1,    0,    0,    1,    0,    0,    0,    0)
+minor_chord <-
+  c(   1,    0,    0,    1,    0,    0,    0,    1,    0,    0,    0,    0)
+seventh_chord <-
+  c(   1,    0,    0,    0,    1,    0,    0,    1,    0,    0,    1,    0)
+
+major_key <-
+  c(6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88)
+minor_key <-
+  c(6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17)
+
+chord_templates <-
+  tribble(
+    ~name, ~template,
+    "Gb:7", circshift(seventh_chord, 6),
+    "Gb:maj", circshift(major_chord, 6),
+    "Bb:min", circshift(minor_chord, 10),
+    "Db:maj", circshift(major_chord, 1),
+    "F:min", circshift(minor_chord, 5),
+    "Ab:7", circshift(seventh_chord, 8),
+    "Ab:maj", circshift(major_chord, 8),
+    "C:min", circshift(minor_chord, 0),
+    "Eb:7", circshift(seventh_chord, 3),
+    "Eb:maj", circshift(major_chord, 3),
+    "G:min", circshift(minor_chord, 7),
+    "Bb:7", circshift(seventh_chord, 10),
+    "Bb:maj", circshift(major_chord, 10),
+    "D:min", circshift(minor_chord, 2),
+    "F:7", circshift(seventh_chord, 5),
+    "F:maj", circshift(major_chord, 5),
+    "A:min", circshift(minor_chord, 9),
+    "C:7", circshift(seventh_chord, 0),
+    "C:maj", circshift(major_chord, 0),
+    "E:min", circshift(minor_chord, 4),
+    "G:7", circshift(seventh_chord, 7),
+    "G:maj", circshift(major_chord, 7),
+    "B:min", circshift(minor_chord, 11),
+    "D:7", circshift(seventh_chord, 2),
+    "D:maj", circshift(major_chord, 2),
+    "F#:min", circshift(minor_chord, 6),
+    "A:7", circshift(seventh_chord, 9),
+    "A:maj", circshift(major_chord, 9),
+    "C#:min", circshift(minor_chord, 1),
+    "E:7", circshift(seventh_chord, 4),
+    "E:maj", circshift(major_chord, 4),
+    "G#:min", circshift(minor_chord, 8),
+    "B:7", circshift(seventh_chord, 11),
+    "B:maj", circshift(major_chord, 11),
+    "D#:min", circshift(minor_chord, 3)
+  )
+
+key_templates <-
+  tribble(
+    ~name, ~template,
+    "Gb:maj", circshift(major_key, 6),
+    "Bb:min", circshift(minor_key, 10),
+    "Db:maj", circshift(major_key, 1),
+    "F:min", circshift(minor_key, 5),
+    "Ab:maj", circshift(major_key, 8),
+    "C:min", circshift(minor_key, 0),
+    "Eb:maj", circshift(major_key, 3),
+    "G:min", circshift(minor_key, 7),
+    "Bb:maj", circshift(major_key, 10),
+    "D:min", circshift(minor_key, 2),
+    "F:maj", circshift(major_key, 5),
+    "A:min", circshift(minor_key, 9),
+    "C:maj", circshift(major_key, 0),
+    "E:min", circshift(minor_key, 4),
+    "G:maj", circshift(major_key, 7),
+    "B:min", circshift(minor_key, 11),
+    "D:maj", circshift(major_key, 2),
+    "F#:min", circshift(minor_key, 6),
+    "A:maj", circshift(major_key, 9),
+    "C#:min", circshift(minor_key, 1),
+    "E:maj", circshift(major_key, 4),
+    "G#:min", circshift(minor_key, 8),
+    "B:maj", circshift(major_key, 11),
+    "D#:min", circshift(minor_key, 3)
+  )
